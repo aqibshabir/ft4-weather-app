748fa70d33c089ded5244309a2f7c6c1982ae2f4
aqibshabir <aqibfaisalshabir@gmail.com>
2023-10-20 10:34:38 +0100

    stored countries in array, currently implementing button that creates 2 random GET requests from array to openWeatherAPI

---
+++ getCountries.js
@@ -0,0 +1,19 @@
+import { countries } from "./countries.js";
+
+export const getRandomCountries = async () => {
+  const rand1 = countries[Math.floor(Math.random() * countries.length)];
+  const rand2 = countries[Math.floor(Math.random() * countries.length)];
+  console.log(rand1, rand2);
+
+  try {
+    const randWeather1 = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${rand1.latlng[0]}&lon=${rand1.latlng[1]}&appid=19a91654c2866453d454714d0821523a`
+    );
+    const randWeather2 = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${rand2.latlng[0]}&lon=${rand2.latlng[1]}&appid=19a91654c2866453d454714d0821523a`
+    );
+    console.log(randWeather1, randWeather2);
+  } catch (e) {
+    console.log("error:", e);
+  }
+};
--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 import { getWeather } from "./geoController.js";
 import { getUserWeather } from "./userController.js";
+import { getRandomCountries } from "./getCountries.js";
 
 const getLocationRef = document.getElementById("getLocation");
+
+const startGameRef = document.getElementById("startGame");
 
 getLocationRef.addEventListener("click", () => {
   getWeather();
 });
 
 getUserWeather();
+
+startGameRef.addEventListener("click", () => {
+  getRandomCountries();
+});
